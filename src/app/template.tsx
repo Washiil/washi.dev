2b77c93c02f254fa5d6f8a93c06d2219c1d18112
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-24 11:15:00 -0500

    added rudimentary page transitions using framer-motion

---
+++ src/app/template.tsx
@@ -0,0 +1,18 @@
+// app/template.tsx
+"use client";
+
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+export default function Template({ children }: { children: ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0.25, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0.25, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
+      {children}
+    </motion.div>
+  );
+}
