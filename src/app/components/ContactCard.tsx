7086981670767e1cc68bba237311978c7cd33c07
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-25 09:33:09 -0500

    Clickable svgs

--- src/app/components/ContactCard.tsx
+++ src/app/components/ContactCard.tsx
@@ -18,15 +18,17 @@ const ContactCard = ({ title, link, description, svg, alt }: ContactCardProps) =
   return (
     <LaserBorderBottom className="group">
       <div className="flex h-auto w-auto max-w-xl rounded-lg transition-all">
-        <Image
-          src={svg}
-          alt={alt}
-          className="h-24 w-24 invert transition-all group-hover:scale-125 lg:h-32 lg:w-32"
-        />
+        <Link href={link} className="flex-shrink-0"> {/* Wrap the Image in a Link */}
+          <Image
+            src={svg}
+            alt={alt}
+            className="h-24 w-24 invert transition-all group-hover:scale-125 lg:h-32 lg:w-32"
+          />
+        </Link>
         <div className="m-5 flex flex-col">
           <Link
             href={link}
-            className="underline-offset-4 transition-all hover:text-purple-600 hover:underline"
+            className="underline-offset-4 transition-all group-hover:text-purple-600 hover:underline"
           >
             <p className="text-4xl">{title} 🔗</p>
           </Link>
