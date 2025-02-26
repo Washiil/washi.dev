a3c0edcce0a7d351b608c5bc5d8671410c708519
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-26 12:04:45 -0500

    added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/NavigationBar"
+import GlitchText from "./components/GlitchedText";
+// app/not-found.tsx
+export default function NotFound() {
+    return (
+        <div>
+            <Navbar/>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <div className="text-7xl font-mono">
+                    <GlitchText text="404"/>
+                </div>
+
+                <div className="h-[1px] w-1/3 my-5 bg-gradient-to-r from-transparent via-purple-600 to-transparent"/>
+
+                <h1 className="text-4xl font-mono">Page Not Found</h1>
+            </div>
+        </div>
+    );
+  }
