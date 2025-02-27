29a7c654f8258b43daf8fc4a74e9d4f9b4388068
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-26 19:20:53 -0500

    prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,19 @@
-import Navbar from "./components/NavigationBar"
-import GlitchText from "./components/GlitchedText";
+import Navbar from './components/NavigationBar';
+import GlitchText from './components/GlitchedText';
 
 export default function NotFound() {
-    return (
-        <div>
-            <Navbar/>
-            <div className="flex flex-col items-center justify-center h-screen">
-                <div className="text-7xl font-mono">
-                    <GlitchText text="404"/>
-                </div>
-
-                <div className="h-[1px] w-1/3 my-5 bg-gradient-to-r from-transparent via-purple-600 to-transparent"/>
-
-                <h1 className="text-4xl font-mono">Page Not Found</h1>
-            </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="flex h-screen flex-col items-center justify-center">
+        <div className="font-mono text-7xl">
+          <GlitchText text="404" />
         </div>
-    );
-  }
+
+        <div className="my-5 h-[1px] w-1/3 bg-gradient-to-r from-transparent via-purple-600 to-transparent" />
+
+        <h1 className="font-mono text-4xl">Page Not Found</h1>
+      </div>
+    </div>
+  );
+}
