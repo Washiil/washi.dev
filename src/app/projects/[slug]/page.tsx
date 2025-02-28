65f07e4b855c7d6c2aeccad1438993dbb85977fd
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-28 17:52:32 -0500

    added video embed and fixed project .mdx formatting

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function Project({ params }: { params: Promise<{ slug: stri
       <div className="flex min-h-screen flex-col items-center justify-center">
         <Header project={metadata} />
         <div className="my-14 h-[1px] w-full bg-gradient-to-r from-transparent via-white to-transparent" />
-        <article className="mdx-content flex max-w-[54rem] flex-col items-center justify-center font-mono backdrop-blur-xl">
+        <article className="mdx-content flex max-w-[54rem] flex-col font-mono backdrop-blur-xl">
           {content.content}
         </article>
       </div>
